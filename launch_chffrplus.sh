dc516f6b1b590dea856609b6fe7c65257b06bc08
HsinTung Chen(JP) <seanchen5716@gmail.com>
2021-04-07 16:35:20 +0800

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -14,11 +14,11 @@ if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     mount -o ro,remount /system
 fi
 
-if [ "$(getprop persist.sys.locale)" != "zh-Hant-TW" ]; then
-    setprop persist.sys.locale zh-Hant-TW
+if [ "$(getprop persist.sys.locale)" != "ko-KR" ]; then
+    setprop persist.sys.locale ko-KR
     setprop persist.sys.language zh
     setprop persist.sys.country TW
-    setprop persist.sys.timezone Asia/Seoul
+    setprop persist.sys.timezone Asia/Taiwan
 
     sleep 2
     #reboot
